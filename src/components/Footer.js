1154e4cf0e27f888b532c8c200922b674f6dbf6e
Schildev <schildscans@gmail.com>
2025-01-17 23:31:08 +0100

    Footer Change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,7 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <footer>
-            <h3>Support Me</h3>
-            <div className="cryptos">
-                <div className="crypto">
-                    <Image width={50} height={50} quality={100} alt={"Ethereum logo"} src={"/ether.png"} />
-                    <span>0x4943eEDc99A7DB06e146717CDaE739c92501a32E</span>
-                </div>
-                <div className="crypto">
-                    <Image width={50} height={50} alt={"usdc logo"} src={"/usdc.png"} quality={100} />
-                    <span>0x4943eEDc99A7DB06e146717CDaE739c92501a32E</span>
-                </div>
-                <div className="crypto">
-                    <Image width={50} height={50} alt={"theter usd logo"} src={"/tusd.png"} quality={100} />
-                    <span>0x4943eEDc99A7DB06e146717CDaE739c92501a32E</span>
-                </div>            
-            </div>
+            <h3>© Schildscans 2025</h3>
         </footer>
     );
 }
